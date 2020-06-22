10a9ae1da8528868b78e87726642bf89df7fd6e5
zFavorite23 <248122551@qq.com>
2020-06-22 16:21:01 +0800

    验证码切换

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,104 +1,41 @@
 <template>
     <div class="login-container">
-        <el-form
-            class="card-box login-form"
-            autoComplete="on"
-            :model="ruleForm"
-            :rules="rules"
-            ref="ruleForm"
-            label-position="left"
-        >
+        <el-form class="card-box login-form" autoComplete="on" :model="ruleForm" :rules="rules" ref="ruleForm" label-position="left">
             <h3 class="title">系统登录</h3>
             <el-form-item prop="username" class="item" style="height: 40px">
-                <el-input
-                    placeholder="姓名或手机号"
-                    name="username"
-                    autoComplete="off"
-                    v-model="ruleForm.username"
-                >
-                    <i slot="prefix" class="el-input__icon"
-                        ><icon-svg icon-class="yonghu"
-                    /></i>
+                <el-input placeholder="姓名或手机号" name="username" autoComplete="off" v-model="ruleForm.username">
+                    <i slot="prefix" class="el-input__icon"><icon-svg icon-class="yonghu" /></i>
                 </el-input>
             </el-form-item>
             <el-form-item prop="password" class="item" style="height: 40px">
-                <el-input
-                    placeholder="密码"
-                    name="pwd"
-                    :type="isShowPwd ? 'text' : 'password'"
-                    @keyup.enter.native="handleLogin"
-                    v-model="ruleForm.password"
-                    autoComplete="off"
-                >
-                    <i slot="prefix" class="el-input__icon"
-                        ><icon-svg icon-class="mima"
-                    /></i>
-                    <i
-                        slot="suffix"
-                        class="el-input__icon"
-                        @click="isShowPwd = !isShowPwd"
-                        ><icon-svg icon-class="zu"
-                    /></i>
+                <el-input placeholder="密码" name="pwd" :type="isShowPwd ? 'text' : 'password'" @keyup.enter.native="handleLogin" v-model="ruleForm.password" autoComplete="off">
+                    <i slot="prefix" class="el-input__icon"><icon-svg icon-class="mima" /></i>
+                    <i slot="suffix" class="el-input__icon" @click="isShowPwd = !isShowPwd"><icon-svg icon-class="zu" /></i>
                 </el-input>
             </el-form-item>
             <el-form-item prop="code" style="height: 40px">
                 <el-row :span="24">
                     <el-col :span="16">
-                        <el-input
-                            :maxlength="code.len"
-                            v-model="ruleForm.code"
-                            size="small"
-                            auto-complete="off"
-                            placeholder="请输入验证码"
-                        >
-                            <i slot="prefix" class="el-input__icon"
-                                ><icon-svg icon-class="mima"
-                            /></i>
+                        <el-input :maxlength="code.len" v-model="ruleForm.code" size="small" auto-complete="off" placeholder="请输入验证码">
+                            <i slot="prefix" class="el-input__icon"><icon-svg icon-class="mima" /></i>
                         </el-input>
                     </el-col>
                     <el-col :span="8">
                         <div class="login-code">
-                            <span
-                                v-if="code.type == 'text'"
-                                class="login-code-img"
-                                >{{ code.value }}</span
-                            >
-                            <img
-                                v-else
-                                :src="code.src"
-                                class="login-code-img"
-                            />
+                            <span v-if="code.type == 'text'" class="login-code-img">{{ code.value }}</span>
+                            <img @click="changeImgCode" v-else :src="code.src" class="login-code-img" />
                         </div>
                     </el-col>
                 </el-row>
             </el-form-item>
 
-            <el-form-item
-                prop="code"
-                style="margin-left: 1px;border: 0; background: none; height: 40px"
-            >
-                <el-checkbox v-model="remember" @change="isRemember"
-                    >记住密码</el-checkbox
-                >
-                <el-checkbox v-model="autoLogin" :disabled="isAutoLogin"
-                    >自动登陆</el-checkbox
-                >
+            <el-form-item prop="code" style="margin-left: 1px;border: 0; background: none; height: 40px">
+                <el-checkbox v-model="remember" @change="isRemember">记住密码</el-checkbox>
+                <el-checkbox v-model="autoLogin" :disabled="isAutoLogin">自动登陆</el-checkbox>
             </el-form-item>
-            <div>
-                <el-button
-                    type="primary"
-                    style="width:100%;margin-bottom:25px;"
-                    :loading="loading"
-                    @click.native="handleLogin()"
-                    >登录
-                </el-button>
-            </div>
+            <div><el-button type="primary" style="width:100%;margin-bottom:25px;" :loading="loading" @click.native="handleLogin()">登录</el-button></div>
             <div v-if="isMobile" style="text-align: center">
-                <img
-                    src="../../assets/image/wx.png"
-                    style="width: 18%;"
-                    @click="wxLogin()"
-                />
+                <img src="../../assets/image/wx.png" style="width: 18%;" @click="wxLogin()" />
                 <!--                <img src="../../assets/image/qq.png" style="width: 18%;float: right;right: 0;margin-right: 20%"/>-->
                 <!--                <el-button type="primary" style="width:100%;margin-bottom:30px;"-->
                 <!--                           @click='showDialog = true'>-->
@@ -108,24 +45,25 @@
         </el-form>
 
         <el-dialog title="第三方验证" :visible.sync="showDialog">
-            邮箱登录成功,请选择第三方验证<br />
+            邮箱登录成功,请选择第三方验证
+            <br />
         </el-dialog>
     </div>
 </template>
 
 <script>
-import { randomLenNum } from "@/utils/utils";
-import { codeUrl } from "@/config/app";
-const Base64 = require("js-base64").Base64;
+import { randomLenNum } from '@/utils/utils';
+import { codeUrl } from '@/config/app';
+const Base64 = require('js-base64').Base64;
 export default {
-    name: "login",
+    name: 'login',
     data() {
         return {
             ruleForm: {
-                username: "", //用户名
-                password: "",
-                code: "",
-                randomStr: ""
+                username: '', //用户名
+                password: '',
+                code: '',
+                randomStr: ''
             },
             remember: true,
             autoLogin: true,
@@ -133,30 +71,25 @@ export default {
             isMobile: false,
             checked: true,
             code: {
-                src: "/code",
-                value: "",
+                src: '/code',
+                value: '',
                 len: 4,
-                type: "image"
+                type: 'image'
             },
             rules: {
-                username: [
-                    { required: true, message: "请输入用户名", trigger: "blur" }
-                ],
-                password: [
-                    { required: true, message: "请输入密码", trigger: "blur" },
-                    { min: 6, message: "密码长度最少为6位", trigger: "blur" }
-                ],
+                username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+                password: [{ required: true, message: '请输入密码', trigger: 'blur' }, { min: 6, message: '密码长度最少为6位', trigger: 'blur' }],
                 code: [
                     {
                         required: true,
-                        message: "请输入验证码",
-                        trigger: "blur"
+                        message: '请输入验证码',
+                        trigger: 'blur'
                     },
                     {
                         min: 4,
                         max: 4,
-                        message: "验证码长度为4位",
-                        trigger: "blur"
+                        message: '验证码长度为4位',
+                        trigger: 'blur'
                     }
                 ]
             },
@@ -180,10 +113,8 @@ export default {
         // let ddmsusername = this.getCookie("ddmsusername");
         // let ddmspassword = Base64.decode(this.getCookie("ddmspassword"));
 
-        let ddmsusername = window.localStorage.getItem("ddmsusername");
-        let ddmspassword = Base64.decode(
-            window.localStorage.getItem("ddmspassword")
-        );
+        let ddmsusername = window.localStorage.getItem('ddmsusername');
+        let ddmspassword = Base64.decode(window.localStorage.getItem('ddmspassword'));
         if (ddmsusername) {
             this.ruleForm.username = ddmsusername;
             this.ruleForm.password = ddmspassword;
@@ -194,6 +125,10 @@ export default {
         //alert(this.getCode().code);
     },
     methods: {
+        changeImgCode() {
+            this.refreshCode()
+        },
+
         isRemember(val) {
             console.log(val);
             if (!val) {
@@ -205,17 +140,15 @@ export default {
             }
         },
         refreshCode(ddmsusername) {
-            this.ruleForm.code = "";
+            this.ruleForm.code = '';
             this.ruleForm.randomStr = randomLenNum(this.code.len, true);
             if (ddmsusername) {
-                this.code.type = "text";
+                this.code.type = 'text';
             }
-            this.code.type === "text"
-                ? (this.code.value = randomLenNum(this.code.len))
-                : (this.code.src = `${this.codeUrl}?randomStr=${this.ruleForm.randomStr}`);
+            this.code.type === 'text' ? (this.code.value = randomLenNum(this.code.len)) : (this.code.src = `${this.codeUrl}?randomStr=${this.ruleForm.randomStr}`);
             this.ruleForm.code = this.code.value;
             //let ddmsAutoLogin = this.getCookie("ddmsAutoLogin");
-            let ddmsAutoLogin = window.localStorage.getItem("ddmsAutoLogin");
+            let ddmsAutoLogin = window.localStorage.getItem('ddmsAutoLogin');
             if (ddmsAutoLogin) {
                 this.login();
             }
@@ -233,8 +166,9 @@ export default {
         login() {
             this.loading = true;
             this.$store
-                .dispatch("loginName", this.ruleForm)
+                .dispatch('loginName', this.ruleForm)
                 .then(response => {
+                    console.log(response);
                     this.loading = false;
                     // if (response.code) {
                     //     this.$message.error(response.message);
@@ -242,7 +176,7 @@ export default {
                     // }
                     // 储存登录信息
                     this.setUserInfo();
-                    let path = "/";
+                    let path = '/';
                     // if (this.redirect) {
                     //     path = this.redirect;
                     // }
@@ -272,7 +206,7 @@ export default {
             // url = 'https://open.weixin.qq.com/connect/qrconnect?appid=' + appid + '&redirect_uri=' + redirect_uri + '&state=WX-BIND&response_type=code&scope=snsapi_login#wechat_redirect'
 
             let url =
-                "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxce4bd89765319e92&redirect_uri=http%3A%2F%2Foa.dreamdeck.cn%2F#%2Flogin&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
+                'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxce4bd89765319e92&redirect_uri=http%3A%2F%2Foa.dreamdeck.cn%2F#%2Flogin&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect';
 
             window.location.href = url;
         },
@@ -289,11 +223,11 @@ export default {
         },
         getCode() {
             const href = location.href;
-            if (href.indexOf("?") != -1) {
-                var search = href.split("?")[1].split("&"),
+            if (href.indexOf('?') != -1) {
+                var search = href.split('?')[1].split('&'),
                     data = {};
                 for (var i = 0; i < search.length; i++) {
-                    var item = search[i].split("=");
+                    var item = search[i].split('=');
                     data[item[0]] = item[1];
                 }
                 return data;
@@ -307,53 +241,46 @@ export default {
             // 判断用户是否勾选记住密码，如果勾选，向cookie中储存登录信息，
             // 如果没有勾选，储存的信息为空
             if (this.remember) {
-                window.localStorage.setItem(
-                    "ddmsusername",
-                    this.ruleForm.username
-                );
+                window.localStorage.setItem('ddmsusername', this.ruleForm.username);
                 //this.setCookie("ddmsusername",this.ruleForm.username)
                 // base64加密密码
                 let passWord = Base64.encode(this.ruleForm.password);
-                window.localStorage.setItem("ddmspassword", passWord);
+                window.localStorage.setItem('ddmspassword', passWord);
                 //this.setCookie("ddmspassword",passWord)
             } else {
-                window.localStorage.setItem("ddmsusername", "");
-                window.localStorage.setItem("ddmspassword", "");
+                window.localStorage.setItem('ddmsusername', '');
+                window.localStorage.setItem('ddmspassword', '');
                 //this.setCookie("ddmsusername","")
                 //this.setCookie("ddmspassword","")
             }
             //this.setCookie("ddmsAutoLogin",this.autoLogin)
-            window.localStorage.setItem("ddmsAutoLogin", this.autoLogin);
+            window.localStorage.setItem('ddmsAutoLogin', this.autoLogin);
         },
         // 获取cookie
         getCookie: function(key) {
             if (document.cookie.length > 0) {
-                var start = document.cookie.indexOf(key + "=");
+                var start = document.cookie.indexOf(key + '=');
                 if (start !== -1) {
                     start = start + key.length + 1;
-                    var end = document.cookie.indexOf(";", start);
+                    var end = document.cookie.indexOf(';', start);
                     if (end === -1) end = document.cookie.length;
                     return unescape(document.cookie.substring(start, end));
                 }
             }
-            return "";
+            return '';
         },
         // 保存cookie
         setCookie: function(cName, value, expiredays) {
             var exdate = new Date();
             exdate.setDate(exdate.getDate() + expiredays);
-            document.cookie =
-                cName +
-                "=" +
-                decodeURIComponent(value) +
-                (expiredays == null ? "" : ";expires=" + exdate.toGMTString());
+            document.cookie = cName + '=' + decodeURIComponent(value) + (expiredays == null ? '' : ';expires=' + exdate.toGMTString());
         }
     }
 };
 </script>
 
 <style type="text/scss" lang="scss">
-@import "../../styles/mixin";
+@import '../../styles/mixin';
 
 $bg: #2d3a4b;
 $dark_gray: #889aa4;
